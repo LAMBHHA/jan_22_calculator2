eb3ae15994ebd842c7c0b6aa8b8c5d9dbb27da93
Gurnam Lambhha <lambhha@sheridancollege.ca>
2019-01-28 17:38:52 -0500

    In class assignment on TDD

---
+++ tests/calculator.py
@@ -0,0 +1,8 @@
+"""
+A calculator library
+"""
+def add(*numbers):
+    total = 0
+    for number in numbers:
+        total += number
+        return total
---
+++ tests/test_add.py
@@ -0,0 +1,27 @@
+"""
+Tests the add() function of the calculator.
+"""
+import calculator
+
+
+def test_two_plus_two():
+    """
+    If given 2 and 2 as parameters, 4 should
+    be returned
+    """
+    assert calculator.add(2, 2) == 4
+
+def test_five_plus_seven():
+    assert calculator.add(5, 7) == 12
+
+def test_no_parameter():
+    """
+    Asserts that no parameters are provided 0 should be returned
+    """
+    assert calculator.add() == 0
+
+def test_with_three_arguments():
+    assert calculator.add(1, 2, 3) == 6
+
+def test_with_five_arguments():
+    assert calculator.add(1, 2, 3, 4, 5) == 15  
